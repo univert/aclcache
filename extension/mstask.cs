dd9d136003749a2cb19a897cfd6f6f2bd7542978
Carter Wang <carter.wang@gmail.com>
2020-05-15 14:36:46 +0800

    MSVC task override

---
+++ extension/mstask.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcadExtension
+{
+    public class CL : Microsoft.Build.CPPTasks.CL
+    {
+        public override bool Execute()
+        {
+            if (null != Environment.GetEnvironmentVariable("_aedebug"))
+            {
+                while (true)
+                {
+                    if (System.Diagnostics.Debugger.IsAttached)
+                    {
+                        System.Diagnostics.Debugger.Break();
+                        break;
+                    }
+
+                    else { }
+                }
+            }
+
+            return base.Execute();
+        }
+    }
+    public class Link : Microsoft.Build.CPPTasks.Link
+    {
+        public override bool Execute()
+        {
+            return base.Execute();
+        }
+    }
+    public class LIB : Microsoft.Build.CPPTasks.LIB
+    {
+        public override bool Execute()
+        {
+            return base.Execute();
+        }
+    }
+}
