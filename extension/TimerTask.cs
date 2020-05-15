dabd6a521bfc2e31ea5918ef2e3dbaca7bece8a6
Carter <carter.wang@gmail.com>
2020-05-15 14:36:46 +0800

    MSVC task override

--- extension/TimerTask.cs
+++ extension/TimerTask.cs
@@ -188,27 +188,5 @@ namespace AcadExtension
                 return true;
             }
         }
-
-        public class CL : Microsoft.Build.CPPTasks.CL
-        {
-            public override bool Execute()
-            {
-                if (null != Environment.GetEnvironmentVariable("_aedebug"))
-                {
-                    while (true)
-                    {
-                        if (System.Diagnostics.Debugger.IsAttached)
-                        {
-                            System.Diagnostics.Debugger.Break();
-                            break;
-                        }
-
-                        else { }
-                    }
-                }
-
-                return base.Execute();
-            }
-        }
     }
 }
