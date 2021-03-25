393a9ae5620f30b8c18ffa120358ee3a8a0af0a2
Carter Wang <carter.wang@gmail.com>
2021-03-25 11:33:16 +0800

    Filter binary path by aclcache_blacklist.txt and upload files
    Filter binary path by aclcache_blacklist.txt and upload files

---
+++ publish/package/compareForAutocad.bat
@@ -0,0 +1,13 @@
+copy /Y %PACK_PATH%\outputfiles1.hash.txt %CI_RES%\outputfiles1.hash.txt
+copy /Y %PACK_PATH%\outputfiles2.hash.txt %CI_RES%\outputfiles2.hash.txt
+copy /Y %PACK_PATH%\aclcache_output_binary_files.txt %CI_RES%\aclcache_output_binary_files.txt
+for /f %%i in (develop\tools\bin\CI\build\aclcache_blacklist.txt) do (
+    findstr /l /v "%%i" %PACK_PATH%\outputfiles1.hash.txt > %temp%\hash1.txt
+    copy %temp%\hash1.txt %PACK_PATH%\outputfiles1.hash.txt
+    findstr /l /v "%%i" %PACK_PATH%\outputfiles2.hash.txt > %temp%\hash2.txt
+    copy %temp%\hash2.txt %PACK_PATH%\outputfiles2.hash.txt
+)
+copy /Y %temp%\hash1.txt %CI_RES%\outputfiles1.hash.filtered.txt
+copy /Y %temp%\hash2.txt %CI_RES%\outputfiles2.hash.filtered.txt
+fc %temp%\hash1.txt %temp%\hash2.txt > "%CI_RES%\compare.result.txt"
+exit /b 0
