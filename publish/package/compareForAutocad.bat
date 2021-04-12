7fdd5f1250784f1528d969aaf555f876fd268804
Carter <carter.wang@gmail.com>
2021-04-12 18:02:31 +0800

    send diff hash file paths to splunk for effective troubleshooting
    * add compare2files.py
    
    * update
    
    * fix
    
    * update compare2files.py
    
    * remove jsonstats argument
    
    * leave a blank space before (
    
    * update name
    
    * add aclcache_blacklist.txt

--- publish/package/compareForAutocad.bat
+++ publish/package/compareForAutocad.bat
@@ -1,7 +1,7 @@
 copy /Y %PACK_PATH%\outputfiles1.hash.txt %CI_RES%\outputfiles1.hash.txt
 copy /Y %PACK_PATH%\outputfiles2.hash.txt %CI_RES%\outputfiles2.hash.txt
 copy /Y %PACK_PATH%\aclcache_output_binary_files.txt %CI_RES%\aclcache_output_binary_files.txt
-for /f %%i in (develop\tools\bin\CI\build\aclcache_blacklist.txt) do (
+for /f %%i in (%~dp0acadTools\aclcache_blacklist.txt) do (
     findstr /l /v "%%i" %PACK_PATH%\outputfiles1.hash.txt > %temp%\hash1.txt
     copy %temp%\hash1.txt %PACK_PATH%\outputfiles1.hash.txt
     findstr /l /v "%%i" %PACK_PATH%\outputfiles2.hash.txt > %temp%\hash2.txt
@@ -9,6 +9,6 @@ for /f %%i in (develop\tools\bin\CI\build\aclcache_blacklist.txt) do (
 )
 copy /Y %temp%\hash1.txt %CI_RES%\outputfiles1.hash.filtered.txt
 copy /Y %temp%\hash2.txt %CI_RES%\outputfiles2.hash.filtered.txt
-fc %temp%\hash1.txt %temp%\hash2.txt > "%CI_RES%\compare.result.txt"
+call python3 %~dp0acadTools\compare2files.py -f1 %temp%\hash1.txt -f2 %temp%\hash2.txt > "%CI_RES%\compare.result.txt"
 call %~dp0acadTools\aclcache_validate.bat
 exit /b 0
